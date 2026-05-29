4d7ed2afca9e7c27b5e95189e2e3a5efd507f0da
RaiDivyansh08 <152912883+RaiDivyansh08@users.noreply.github.com>
2026-05-30 01:12:18 +0530

    Setup Tailwind CSS

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,13 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
 import './App.css'
 
 function App() {
   return (
-    <h1 className="text-4xl font-bold">
-      IngredientInsight
-    </h1>
+    <div className="min-h-screen flex items-center justify-center">
+      <h1 className="text-5xl font-bold">
+        IngredientInsight 🚀
+      </h1>
+    </div>
   );
 }
 
